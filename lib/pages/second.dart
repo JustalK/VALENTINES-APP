ba8500251e22a3de8573c2e248a2bd8a8e4f72d5
Latsuj <justal.kevin@gmail.com>
2021-01-16 23:30:13 +0800

    [FEATURE] Adding a slider img

--- lib/pages/second.dart
+++ lib/pages/second.dart
@@ -6,17 +6,33 @@ class Second extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(Routes().generateRoute(Home(), 800));;
-          },
-          child: Text('Go back!'),
-        ),
-      ),
+      body: Container(
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: (MediaQuery.of(context).size.width),
+                child: new Image.asset('assets/images/me.jpeg')
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.green,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.orange,
+              ),
+            ],
+          ),
+      )
     );
   }
 }
