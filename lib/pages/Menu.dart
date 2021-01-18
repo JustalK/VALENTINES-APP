ab16b262eed7152bf7cf7d5328ecd1ce8864b6f0
Latsuj <justal.kevin@gmail.com>
2021-01-18 22:41:45 +0800

    [FEATURE] Adding a list for the menu

--- lib/pages/Menu.dart
+++ lib/pages/Menu.dart
@@ -8,14 +8,35 @@ class Menu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Background(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Text('TITLE'),
-            )
-          ],
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 50,
+          padding: EdgeInsets.symmetric(vertical: 40.0),
+          children: List.generate(7, (index) {
+            return Column(
+              children: <Widget> [
+                Center(
+                  child: Container(
+                    width: 150.0,
+                    height: 150.0,
+                    decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(5)),
+                    child: Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: new Image.asset('assets/images/me.jpeg')
+                    )
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: Text(
+                    'Item $index',
+                    style: Theme.of(context).textTheme.headline5,
+                  )
+                )
+              ]
+            );
+          })
         )
       )
     );
