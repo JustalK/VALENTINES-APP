2cd03a4573c2f66bcc23568db85c9bd4b5881c17
Latsuj <justal.kevin@gmail.com>
2021-01-18 23:25:08 +0800

    [FEATURE] Fixing the Column/GridView with the ListView

--- lib/pages/Menu.dart
+++ lib/pages/Menu.dart
@@ -8,35 +8,40 @@ class Menu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Background(
-        child: GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 50,
-          padding: EdgeInsets.symmetric(vertical: 40.0),
-          children: List.generate(7, (index) {
-            return Column(
-              children: <Widget> [
-                Center(
-                  child: Container(
-                    width: 150.0,
-                    height: 150.0,
-                    decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(5)),
-                    child: Padding(
-                      padding: EdgeInsets.all(5.0),
-                      child: new Image.asset('assets/images/me.jpeg')
-                    )
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0),
-                  child: Text(
-                    'Item $index',
-                    style: Theme.of(context).textTheme.headline5,
-                  )
-                )
-              ]
-            );
-          })
+        child: ListView(
+          children: <Widget>[
+            Center(
+              child: Text('Title'),
+            ),
+            GridView.count(
+                crossAxisCount: 2,
+                physics: ScrollPhysics(), // to disable GridView's scrolling
+                shrinkWrap: true,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 50,
+                padding: EdgeInsets.symmetric(vertical: 40.0),
+                children: List.generate(7, (index) {
+                  return Column(
+                    children: <Widget> [
+                      Center(
+                        child: CircleAvatar(
+                          radius: 72.0,
+                          backgroundColor: Colors.transparent,
+                          backgroundImage: AssetImage('assets/images/me.jpeg'),
+                        )
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                        child: Text(
+                          'Item $index',
+                          style: Theme.of(context).textTheme.headline5,
+                        )
+                      )
+                    ]
+                  );
+                })
+              )
+          ]
         )
       )
     );
