bd91b1a2476da116cad161fdb213bacab956ce23
Latsuj <justal.kevin@gmail.com>
2021-01-21 00:15:02 +0800

    [FEATURE] Adding the list as dynamic for the menus

--- lib/pages/Menu.dart
+++ lib/pages/Menu.dart
@@ -4,7 +4,36 @@ import 'BigMemory.dart';
 import 'History.dart';
 import '../components/Background.dart';
 
+class MenuObject {
+  String path;
+  String title;
+  Widget widget;
+
+  MenuObject(this.path, this.title, this.widget);
+
+  String get getPath {
+    return this.path;
+  }  
+
+  String get getTitle {
+    return this.title;
+  }
+
+  Widget get getWidget {
+    return this.widget;
+  }
+}
+
 class Menu extends StatelessWidget {
+  List<MenuObject> menus = [
+    MenuObject('assets/images/me.jpeg', 'TITLE', History()),
+    MenuObject('assets/images/me.jpeg', 'TITLE', BigMemory()),
+    MenuObject('assets/images/me.jpeg', 'TITLE', History()),
+    MenuObject('assets/images/me.jpeg', 'TITLE', History()),
+    MenuObject('assets/images/me.jpeg', 'TITLE', History()),
+    MenuObject('assets/images/me.jpeg', 'TITLE', History()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,25 +50,25 @@ class Menu extends StatelessWidget {
                 crossAxisSpacing: 8,
                 mainAxisSpacing: 50,
                 padding: EdgeInsets.symmetric(vertical: 40.0),
-                children: List.generate(7, (index) {
+                children: List.generate(menus.length, (i) {
                   return Column(
                     children: <Widget> [
                       Center(
                         child: InkWell(
                           onTap: () {
-                            Navigator.of(context).push(Routes().generateRoute(History(), 800));
+                            Navigator.of(context).push(Routes().generateRoute(menus[i].widget, 800));
                           },
                           child: CircleAvatar(
                             radius: 72.0,
                             backgroundColor: Colors.transparent,
-                            backgroundImage: AssetImage('assets/images/me.jpeg'),
+                            backgroundImage: AssetImage(menus[i].path),
                           )
                         )
                       ),
                       Padding(
                         padding: EdgeInsets.only(top: 20.0),
                         child: Text(
-                          'Item $index',
+                          menus[i].title,
                           style: Theme.of(context).textTheme.headline5,
                         )
                       )
